36d6ed6acbcb681ec7697a10dd76bbc5a6fa6b81
Parihar07 <73985710+hparihar-07@users.noreply.github.com>
2023-08-05 09:36:11 +0530

    Add files via upload

---
+++ Self_destructors.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >= 0.7.0 < 0.9.0;
+contract HelloWorld{
+    address owner;
+    constructor(){
+        owner = msg.sender;
+    }
+    receive() external payable{}
+    function destroy() external{
+        require(owner == msg.sender, "You are not the owner");
+        selfdestruct(payable (owner));
+    }
+    function getOwner() public view returns(address){
+         return owner;
+    }
+}
